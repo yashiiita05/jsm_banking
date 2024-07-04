c8987198ae0767263804cdf08c2d9e29851b54df
Yashita Maurya <maurya07yashita@gmail.com>
2024-07-05 01:12:21 +0530

    Implement initial file

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter , IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight:['400','700'],
+  variable: '--font-ibm-plex-serif'
+})
+export const metadata: Metadata = {
+  title: "L.A.S.T. Bank",
+  description: "Luxury Access Services and Trust – Elevating your banking experience with premium services and unwavering reliability",
+  icons: {
+    icon:'/icons/logo.svg'}
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={'${inter.variable} ${ibmflexserif.variable}'}>{children}</body>
+    </html>
+  );
+}
