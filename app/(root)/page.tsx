e3d978e6e04ec034942aca6241072af14052cbcf
Yashita Maurya <maurya07yashita@gmail.com>
2024-07-05 15:41:48 +0530

    implement initinal file and folder structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+
+import HeaderBox from '@/components/ui/HeaderBox'
 
 const Home = () => {
+  const loggedIn ={ firstName: 'Cyra'};
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+
+         <HeaderBox
+         type="greeting"
+         title="Welcome"
+         user={loggedIn?.firstName || 'Guest'}
+         subtext="Access and manage your account and transactions efficiently."
+         />
+      
+        </header>
+
+      </div>
+      
+    </section>
   )
 }
 
